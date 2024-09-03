4cf8a75aab38487b23dbaf7dee954667fc14f4f4
Samanth github <samanthkrishnappa@gmail.com>
2024-09-03 04:25:49 +0000

    added connected callback

--- force-app/main/default/lwc/xPressView/xPressView.js
+++ force-app/main/default/lwc/xPressView/xPressView.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
 
-export default class XPressView extends LightningElement {}
+export default class XPressView extends LightningElement {
+    connectedCallback() {
+        console.log('In Connnect callback');
+    }
+}
